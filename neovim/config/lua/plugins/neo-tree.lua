9d1abe14055ac5bba530ad7bccecceb27bdd3d0f
cyberphysails <physails@outlook.com>
2024-04-26 09:21:19 +0800

    add: modify <tab> key mapping

--- neovim/config/lua/plugins/neo-tree.lua
+++ neovim/config/lua/plugins/neo-tree.lua
@@ -1,9 +1,19 @@
-require("neo-tree").setup({
+local neo_tree = require("neo-tree")
+local fc = require("neo-tree.sources.filesystem.commands")
+neo_tree.setup({
   filesystem = {
     window = {
       -- :h neo-tree-custom-mappings
       mappings = {
-        ["<Tab>"] = "toggle_node",
+--        ["<Tab>"] = "toggle_node",
+        ["<Tab>"] = function (state)
+          local node = state.tree:get_node()
+          if node.type == "directory" then
+            fc.toggle_node(state)
+          else
+            fc.close_node(state)
+          end
+        end
       }
     }
   },
